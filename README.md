8b2597670ca499ab17c103c28e49a972c33d333e
Daniel Toader & Brian Kropff <student@epicodus.com>
2015-04-14 10:57:52 -0700

    initialize repository

---
+++ README.md
@@ -0,0 +1,22 @@
+# TamagotchiJS for Epicodus
+## by Daniel Toader and Brian Kropff
+### Date: April 14, 2015
+#### Description
+This web app makes a cute tamagotchi.
+
+#### Setup instructions
+1. Clone this git repository
+2. Start the web app by loading tamagotchi.html in your browser
+
+#### Copyright © 2015, Daniel Toader and Brian Kropff
+
+#### License: [MIT](https://github.com/twbs/bootstrap/blob/master/LICENSE)
+
+#### Technologies used
+- HTML5
+- CSS3
+- JavaScript
+- JQuery ver 2.1.3
+- Bootstrap ver 3.3.1
+- mochaJS ver 2.2.1
+- chaiJS ver 2.2.0
---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+jQuery(document).ready(function() {
+  $("#sideA").focus();
+  $("#triangle").submit(function(event) {
+    $("#warning").hide();
+    var newSideA = parseInt($("#sideA").val());
+    var newSideB = parseInt($("#sideB").val());
+    var newSideC = parseInt($("#sideC").val());
+    // var result = getTriangle(sideA, sideB, sideC);
+    var newTriangle = {
+      sideA: newSideA,
+      sideB: newSideB,
+      sideC: newSideC,
+
+      printTriangle: function() {
+        return "<li>" + this.sideA + ", " + this.sideB + ", "+ this.sideC + "</li>";
+      },
+
+      getTriangle: function() {
+        // sides cannot be 0 and one side cannot be longer than the other
+        // two combined
+        if (this.sideA === 0 || this.sideB === 0 || this.sideC === 0 ||
+            this.sideA + this.sideB < this.sideC ||
+            this.sideB + this.sideC < this.sideA ||
+            this.sideA + this.sideC < this.sideB) {
+          $("#print-message").text("Not a triangle!");
+          $("#message").show();
+        } else if (this.sideA === this.sideB && this.sideA === this.sideC) {
+          $(".equilateral").append(this.printTriangle());
+          $("#print-message").text("Equilateral!");
+          $("#message").show();
+        } else if (this.sideA === this.sideB || this.sideA === this.sideC || this.sideB ===this.sideC) {
+          $(".isosceles").append(this.printTriangle());
+          $("#print-message").text("Isosceles!");
+          $("#message").show();
+        } else {
+          $(".scalene").append(this.printTriangle());
+          $("#print-message").text("Scalene!");
+          $("#message").show();
+        }
+      }
+    }
+    newTriangle.getTriangle();
+
+    event.preventDefault();
+  });
+});
